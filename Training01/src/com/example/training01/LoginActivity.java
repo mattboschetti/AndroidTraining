db624da6e0625bed18af78240222b4a0db0f7164
mattboschetti <mattboschetti@gmail.com>
2014-01-09 19:31:37 -0200

    Renaming of MainActivity to LoginActivity

--- Training01/src/com/example/training01/LoginActivity.java
+++ Training01/src/com/example/training01/LoginActivity.java
@@ -11,7 +11,7 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	private int started;
 	private int stopped;
@@ -21,9 +21,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		
+		setContentView(R.layout.activity_login);
 		
 		findViewById(R.id.btnOk).setOnClickListener(new OnClickListener() {
 			
@@ -31,7 +29,7 @@ public class MainActivity extends Activity {
 			public void onClick(View arg0) {
 				EditText txtUser = (EditText) findViewById(R.id.txtUser);
 				EditText txtPwd = (EditText) findViewById(R.id.txtPassword);
-				String user = txtUser.getText() != null ? txtUser.getText().toString() : "Banzo";
+				String user = txtUser.getText() != null ? txtUser.getText().toString() : "Bazinga";
 				String pwd = txtPwd.getText() != null ? txtPwd.getText().toString() : "Pwd";
 				
 				saveUserAndPassword(user, pwd);
@@ -79,18 +77,14 @@ public class MainActivity extends Activity {
 	
 	@Override
 	protected void onStop() {
-		// TODO Auto-generated method stub
 		super.onStop();
-		System.out.println("Stopped: " + stopped + " times");
 		Toast.makeText(getApplicationContext(), "Stopped " + stopped + " times", Toast.LENGTH_SHORT).show();
 		stopped++;
 	}
 	
 	@Override
 	protected void onStart() {
-		// TODO Auto-generated method stub
 		super.onStart();
-		System.out.println("Started: " + started + " times");
 		Toast.makeText(getApplicationContext(), "Started " + started + " times", Toast.LENGTH_SHORT).show();
 		started++;
 		
@@ -102,7 +96,6 @@ public class MainActivity extends Activity {
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
