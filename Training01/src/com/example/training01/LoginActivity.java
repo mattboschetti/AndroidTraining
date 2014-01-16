aa83e18ca2011ccf9f170661c27ae023c12db506
mattboschetti <mattboschetti@gmail.com>
2014-01-15 23:09:04 -0200

    Adding support for async http request

--- Training01/src/com/example/training01/LoginActivity.java
+++ Training01/src/com/example/training01/LoginActivity.java
@@ -1,18 +1,39 @@
 package com.example.training01;
 
-import com.example.training01.LoginFragment.OnLoginClickListener;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.widget.Toast;
 
+import com.example.training01.LoginFragment.OnLoginClickListener;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 public class LoginActivity extends FragmentActivity implements OnLoginClickListener {
 
+	private static AsyncHttpClient client = new AsyncHttpClient();
+	
 	@Override
 	public void onClick(String message) {
 		UIUtils.toast(message, getApplicationContext());
+	}
+	
+	private void requestSiteDoDeivison() {
+		RequestParams params = new RequestParams();
+		params.add("gender", "m");
+		client.post("http://www.dueloamais.com.br:8080/dueloamais-api/ws/tag/list", params, new AsyncHttpResponseHandler() {
+		    @Override
+		    public void onSuccess(String response) {
+		    	UIUtils.toast(response.substring(1, response.length()-1).replaceAll("\"", ""), getApplicationContext());
+		    }
+		    
+		    @Override
+		    public void onFailure(Throwable error) {
+		    	UIUtils.toast(error.getMessage(), getApplicationContext());
+		    }
+		});
 	}
 	
 	private int started;
@@ -23,7 +44,7 @@ public class LoginActivity extends FragmentActivity implements OnLoginClickListe
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		openLoginFragment();
-		
+		requestSiteDoDeivison();
 		started = 1;
 		stopped = 1;
 		
